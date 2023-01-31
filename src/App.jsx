29d7e11b952080c3eedf7d913732ddee13b8210f
Jasper Wesseling <131478@student.drenthecollege.nl>
2023-01-31 09:02:57 +0100

    install sass

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './css/App.scss'
+
+function App() {
+	const [count, setCount] = useState(0)
+	
+	return (
+		<div className='test'>
+			<p>test</p>
+		</div>
+	)
+}
+	
+export default App
+	
